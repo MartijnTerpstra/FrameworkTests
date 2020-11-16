a38d2d9e29db7a90e3cc009288bc681193218d33
Martijn Terpstra <m-terpstra@outlook.com>
2020-11-16 10:33:05 +0100

    Pre CI impl

--- UtilityTests/Properties/AssemblyInfo.cs
+++ UtilityTests/Properties/AssemblyInfo.cs
@@ -2,18 +2,18 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("Utility.Tests")]
+[assembly: AssemblyTitle("UtilityTests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Utility.Tests")]
+[assembly: AssemblyProduct("UtilityTests")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 [assembly: ComVisible(false)]
 
-[assembly: Guid("a15bd37a-bfe8-4729-899c-6ed5ab01b0bb")]
+[assembly: Guid("673d26ce-2107-4849-8bd8-87ce4bcd547c")]
 
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
---
+++ UtilityTests/SumToolTests.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Utility;
+
+namespace UtilityTests
+{
+    [TestClass]
+    public class SumToolTests
+    {
+        [TestMethod]
+        public void SumEmptyShouldReturnZero()
+        {
+            Assert.AreEqual(new SumTool().Sum(Array.Empty<int>()), 0);
+        }
+    }
+}
