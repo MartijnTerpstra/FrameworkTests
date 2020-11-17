010b2537ba25f7c2a50e2f738b601bccea2af43b
MartijnTerpstra <m-terpstra@outlook.com>
2020-11-17 01:21:07 +0100

    should fail

--- UtilityTests/SumToolTests.cs
+++ UtilityTests/SumToolTests.cs
@@ -23,7 +23,7 @@ namespace UtilityTests
         public void SumTwoValuesShouldReturnTheAddition()
         {
             Assert.AreEqual(200, new SumTool().Sum(new[] { 100, 100 }));
-            Assert.AreEqual(0, new SumTool().Sum(new[] { 100, -100 }));
+            Assert.AreEqual(1, new SumTool().Sum(new[] { 100, -100 }));
         }
 
     }
